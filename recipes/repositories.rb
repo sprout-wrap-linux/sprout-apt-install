ecafc0825e3c0c330a129707452be3428bad8896
vagrant <vagrant@vagrant.vm>
2017-04-11 22:10:03 +0000

    install gcloud

--- recipes/repositories.rb
+++ recipes/repositories.rb
@@ -2,6 +2,9 @@
 node['sprout']['apt']['repositories'].each do  |repo|
   apt_repository repo['name'] do
     uri repo['uri']
+    key repo['key']
+    distribution repo['distribution']
+    components repo['components']
   end
 end
 
